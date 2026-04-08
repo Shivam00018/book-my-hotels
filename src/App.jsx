3968c21ebc88cf42bacc98ff308d0f40bee4d2d1
Shivam <shivam.ug23@nsut.ac.in>
2026-04-09 00:15:14 +0530

    Add feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Navbar from './component/Navbar'
 import './App.css'
+import { Route ,Routes } from 'react-router-dom'
+import Home from './pagess/Home'
 
 function App() {
  
 
   return (
     <>
+    <div>
      <Navbar/>
+     <div className='min-h-[70vh]' >
+       <Routes>
+        <Route path='/' element = {<Home/>}/>
+       </Routes>
+     </div>
+     </div>
     </>
   )
 }
---
+++ src/pagess/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../component/Hero'
+import FeatureDestination from '../component/FeatureDestination'
+
+const Home = () => {
+  return (
+    <>
+        <Hero/>
+        <FeatureDestination/>
+    </>
+  )
+}
+
+export default Home
